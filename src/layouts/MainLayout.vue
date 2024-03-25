e93ec3ba039629bdc1735e4a30ac5fa3592d5fc4
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2024-03-25 21:35:48 +0200

    tweaking responsiveness

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,101 +1,79 @@
 <template>
   <q-layout view="hHh lpR fFf" class="bg-white">
+    <!-- header -->
     <q-header unelevated class="bg-white text-black q-py-xs" height-hint="58">
       <q-toolbar>
         <q-btn flat dense round @click="toggleLeftDrawer" aria-label="Menu" icon="menu" />
 
-        <q-btn flat no-caps no-wrap class="" v-if="$q.screen.gt.xs">
+        <q-btn flat no-caps no-wrap class="logo">
           <q-icon :name="fabYoutube" color="red" size="28px" />
           <q-toolbar-title shrink class="text-weight-bold">
             YouTube
           </q-toolbar-title>
         </q-btn>
-
         <q-space />
-
         <div class="YL__toolbar-input-container row no-wrap ">
           <q-input dense outlined rounded v-model="search" placeholder="Search" class="bg-white col ">
             <template v-slot:append>
               <q-icon v-if="search === ''" name="keyboard" />
               <q-icon v-else name="clear" class="cursor-pointer" @click="search = ''" />
-              <q-btn unelevated outlined rounded color="grey-3" text-color="black" icon="search">
+              <q-btn unelevated outlined rounded color="grey-3" class="search-btn" text-color="black" icon="search">
               </q-btn>
             </template>
           </q-input>
 
-
         </div>
-
-
-
         <div class="micro-icon">
           <q-btn unelevated color="white" text-color="black" icon="micro"></q-btn>
         </div>
-
         <q-space />
-
-        <div class="q-gutter-sm row items-center no-wrap">
-
-
-          <q-btn unelevated round color="primary" size="8px" text-color="white" icon="person" />
+        <div class="q-gutter-sm row items-center no-wrap" v-if="$q.screen.gt.xs">
+          <q-btn unelevated round color="bg-white" size="8px" text-color="white">
+            <div class="cutom-dots"> ⫶
+            </div>
+          </q-btn>
           <q-btn outline rounded color="primary" class="q-py-sm">
             <q-btn unelevated round color="primary" size="8px" text-color="white" icon="person" class="q-p" />
             <div class="profile">profile</div>
           </q-btn>
-          <q-btn unelevated round color="primary" text-color="white" size="8px" icon="person" />
+          <q-icon size="20px"><img src="/icons/drop.png" alt="Drop Image" class="drop-icon"></q-icon>
+
         </div>
+        <div v-else>
+          <q-btn unelevated round color="bg-white" size="8px" text-color="white">
+            <div class="cutom-dots"> ⫶
+            </div>
+
+          </q-btn>
+        </div>
+
       </q-toolbar>
     </q-header>
-
-     <q-drawer v-model="leftDrawerOpen"   class="bg-white" :width="200">
+<!-- menu  drawer-->
+    <q-drawer v-if="leftDrawerOpen" v-model="leftDrawerOpen" class="bg-white" :width="200">
       <q-scroll-area class="fit">
         <q-list padding>
           <q-item v-for="link in links1" :key="link.text" v-ripple clickable>
             <q-item-section avatar>
-              <q-icon color="grey" :name="link.icon" />
+              <q-icon color="black" :name="link.icon" />
             </q-item-section>
-            <q-item-section  >
-              <q-item-label>{{ link.text }}</q-item-label>
+            <q-item-section>
+              <q-item-label color="black">{{ link.text }}</q-item-label>
             </q-item-section>
           </q-item>
           <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
             <q-item-section avatar>
-              <q-icon color="grey" :name="link.icon" />
+              <q-icon color="black" :name="link.icon" />
             </q-item-section>
-            <q-item-section >
-              <q-item-label>{{ link.text }}</q-item-label>
+            <q-item-section>
+              <q-item-label color="black">{{ link.text }}</q-item-label>
             </q-item-section>
           </q-item>
         </q-list>
       </q-scroll-area>
-    </q-drawer>  
-
-    <!-- <q-drawer v-model="defautDrawerOpen"   class="bg-white" :width="200">
-        <q-scroll-area class="fit">
-          <q-list padding>
-            <q-item v-for="link in links1" :key="link.text" v-ripple clickable>
-              <q-item-section avatar>
-                <q-icon color="grey" :name="link.icon" />
-              </q-item-section>
-              <q-item-section  >
-                <q-item-label>{{ link.text }}</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
-              <q-item-section avatar>
-                <q-icon color="grey" :name="link.icon" />
-              </q-item-section>
-              <q-item-section >
-                <q-item-label>{{ link.text }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-scroll-area>
-      </q-drawer>   -->
-
-
-
-    <q-page-container >
+    </q-drawer>
+    <!-- page content -->
+    <q-page-container>
       <div class="bg-white ">
         <IndexPage />
       </div>
@@ -107,7 +85,7 @@
 import { ref } from 'vue'
 import IndexPage from '../pages/IndexPage.vue';
 import { fabYoutube } from '@quasar/extras/fontawesome-v6'
-// import { faRaindrop } from '@quasar/extras/fontawesome-v6'
+
 
 
 
@@ -207,8 +185,56 @@ export default {
 }
 
 .q_item {
-    display: flex;
-    flex-direction: column;
-    
+  display: flex;
+  flex-direction: column;
+
 }
-      </style>
+
+.cutom-dots {
+  font-weight: bolder;
+  color: black;
+  font-size: 25px;
+}
+
+.drop-icon {
+  object-fit: cover;
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+@media (max-width: 600px) {
+
+  .q-btn .q-icon,
+  .q-btn .q-spinner {
+    font-size: 16px;
+  }
+
+  .micro-icon {
+    margin: 0px 0px;
+  }
+
+  .q-btn {
+    padding: 0px;
+  }
+
+  .logo {
+    padding: 0px;
+  }
+
+  .q-icon,
+  .material-icons {
+    justify-content: unset;
+  }
+
+  .search-btn {
+    padding: 0px 15px;
+  }
+
+  .q-toolbar__title {
+    font-size: 12px;
+    padding: 0 2px;
+
+  }
+}</style>
